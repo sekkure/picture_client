28a2731f3a5cf18a30f0bdf0c5dd3bb9f76e3006
sekkure <secreori@gmail.com>
2022-06-06 03:57:38 +0300

    fix: move re-auth to layout & add gallery route

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,13 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { useRefreshTokenMutation } from './features/auth/authApiSlice'
-import { setCredentials } from './features/auth/authSlice'
-import { useGetUserByIdMutation } from './features/users/usersApiSlice'
 import RequireAuth from './features/RequireAuth'
 import Layout from './features/Layout'
 import LoginPage from './pages/Login'
 import MainPage from './pages/Main'
 import PreviewPost from './pages/PreviewPost'
+import Gallery from './pages/Gallery'
 
 function App() {
-  const userId = localStorage.getItem('userId')
-
-  const [refreshToken] = useRefreshTokenMutation()
-  const [getUserById] = useGetUserByIdMutation()
-
-  useEffect(() => {
-    if (userId) {
-      if (window.location.pathname === '/login') return
-
-      refreshToken({})
-        .unwrap()
-        .then(res => {
-          getUserById(userId)
-            .unwrap()
-            .then(user => {
-              setCredentials({ user, ...res })
-            })
-            .catch(err => {
-              localStorage.removeItem('userId')
-            })
-        })
-        .catch(err => {
-          localStorage.removeItem('userId')
-        })
-    }
-  }, [userId])
-
   return (
     <BrowserRouter>
       <Routes>
@@ -51,6 +22,7 @@ function App() {
           {/* Public routes */}
 
           <Route path="login" element={<LoginPage />} />
+          <Route path="gallery" element={<Gallery />} />
         </Route>
       </Routes>
     </BrowserRouter>
