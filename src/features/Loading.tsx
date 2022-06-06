7a65c620fa56531c0cbbbb3e8bc435f239a251ba
sekkure <secreori@gmail.com>
2022-06-06 04:03:23 +0300

    feat: funny component of loading screen, why not?

---
+++ src/features/Loading.tsx
@@ -0,0 +1,18 @@
+const Loading = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%',
+        flexDirection: 'column',
+      }}
+    >
+      <img src="https://c.tenor.com/OwqThGTZA2EAAAAi/amogus-spin-amogus.gif" />
+      <h1 style={{ color: 'white', fontWeight: 'bold' }}>Loading...</h1>
+    </div>
+  )
+}
+
+export default Loading
