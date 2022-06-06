4e4bec0a64e7dd5c0ae59ef0f36dde31e5569a27
sekkure <secreori@gmail.com>
2022-06-06 04:03:11 +0300

    feat: add re-auth with loading screen...

--- src/features/Layout.tsx
+++ src/features/Layout.tsx
@@ -1,7 +1,41 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import { Outlet } from 'react-router-dom'
+import { useAppSelector } from '../hooks/redux'
+import { useRefreshTokenMutation } from './auth/authApiSlice'
+import { setCredentials } from './auth/authSlice'
+import Loading from './Loading'
+import { useGetUserByIdMutation } from './users/usersApiSlice'
 
 const Layout = () => {
-  return <Outlet />
+  const user = useAppSelector(state => state.auth.user)
+  const userId = localStorage.getItem('userId')
+  const dispatch = useDispatch()
+
+  const [refreshToken] = useRefreshTokenMutation()
+  const [getUserById] = useGetUserByIdMutation()
+
+  useEffect(() => {
+    if (userId && !user) {
+      refreshToken({})
+        .unwrap()
+        .then(res => {
+          getUserById(userId)
+            .unwrap()
+            .then(user => {
+              dispatch(setCredentials({ user, ...res }))
+            })
+            .catch(err => {
+              localStorage.removeItem('userId')
+            })
+        })
+        .catch(err => {
+          localStorage.removeItem('userId')
+        })
+    }
+  }, [userId])
+
+  return !userId || user ? <Outlet /> : <Loading />
 }
 
 export default Layout
